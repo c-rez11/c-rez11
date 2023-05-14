dd80f4e6bc161fc7bef8c2f7d02293caf8b9a3f4
c-rez11 <111709921+c-rez11@users.noreply.github.com>
2023-05-14 13:22:55 -0500

    Delete ECO_685 directory

--- ECO_685/transaction_volume_pandas1.py
+++
@@ -1,6 +0,0 @@
-
-import json
-import pandas as pd
-with open('C:\\Users\\cresnick\\OneDrive - Epic\\Desktop\\python projects\\Udemy courses\\new_era\\c-rez11\\ECO_685\\transaction_volume1.json', 'r') as f:
-    data = json.load(f)
-df = pd.DataFrame(data)
